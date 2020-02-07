fix loss calculation

mnist_cifar/main.py
@@ -8,17 +8,19 @@ import logging
 import hashlib
 import copy
 
-#=================================================================================
+# =================================================================================
 
 import torch
+import torch.nn as nn  #rana
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 import numpy as np
+from torch import Tensor
 
 import sparselearning
 from sparselearning.core import Masking, CosineDecay, LinearDecay
-from sparselearning.models import AlexNet, VGG16, LeNet_300_100, LeNet_5_Caffe, WideResNet,MobileNet
+from sparselearning.models import AlexNet, VGG16, LeNet_300_100, LeNet_5_Caffe, WideResNet, MobileNet
 from sparselearning.utils import get_mnist_dataloaders, get_cifar10_dataloaders, plot_class_feature_histograms
 
 from extensions import magnitude_variance_pruning, variance_redistribution
@@ -31,8 +33,8 @@ if not os.path.exists('./logs'): os.mkdir('./logs')
 logger = None
 
 models = {}
-models['lenet5'] = (LeNet_5_Caffe,[])
-models['lenet300-100'] = (LeNet_300_100,[])
+models['lenet5'] = (LeNet_5_Caffe, [])
+models['lenet300-100'] = (LeNet_300_100, [])
 models['alexnet-s'] = (AlexNet, ['s', 10])
 models['alexnet-b'] = (AlexNet, ['b', 10])
 models['vgg-c'] = (VGG16, ['C', 10])
@@ -42,7 +44,8 @@ models['wrn-28-2'] = (WideResNet, [28, 2, 10, 0.3])
 models['wrn-22-8'] = (WideResNet, [22, 8, 10, 0.3])
 models['wrn-16-8'] = (WideResNet, [16, 8, 10, 0.3])
 models['wrn-16-10'] = (WideResNet, [16, 10, 10, 0.3])
-models['MobileNet'] = (MobileNet, [1, 100]) # rana:
+models['MobileNet'] = (MobileNet, [1, 100, False])  # rana:
+
 
 def setup_logger(args):
     global logger
@@ -61,7 +64,8 @@ def setup_logger(args):
     args_copy.log_interval = 1
     args_copy.seed = 0
 
-    log_path = './logs/{0}_{1}_{2}.log'.format(args.model, args.density, hashlib.md5(str(args_copy).encode('utf-8')).hexdigest()[:8])
+    log_path = './logs/{0}_{1}_{2}.log'.format(args.model, args.density,
+                                               hashlib.md5(str(args_copy).encode('utf-8')).hexdigest()[:8])
 
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter(fmt='%(asctime)s: %(message)s', datefmt='%H:%M:%S')
@@ -70,10 +74,43 @@ def setup_logger(args):
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
+
 def print_and_log(msg):
     global logger
     print(msg)
     logger.info(msg)
+"""
+def train(args, model, device, train_loader, optimizer, epoch, lr_scheduler, mask=None):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if lr_scheduler is not None: lr_scheduler.step()
+        data, target = data.to(device), target.to(device)
+        if args.fp16: data = data.half()
+        optimizer.zero_grad()
+        output = model(data)
+
+        # rana:
+        loss = F.nll_loss(output, target)
+        # loss = CrossEntropyLoss()
+
+        if args.fp16:
+            optimizer.backward(loss)
+        else:
+            loss.backward()
+
+        if mask is not None:
+            mask.step()
+        else:
+            optimizer.step()
+
+        if batch_idx % args.log_interval == 0:
+            print_and_log('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader) * args.batch_size,
+                       100. * batch_idx / len(train_loader), loss.item()))
+"""
+
+# rana:
+criterion = nn.CrossEntropyLoss()
 
 def train(args, model, device, train_loader, optimizer, epoch, lr_scheduler, mask=None):
     model.train()
@@ -84,20 +121,45 @@ def train(args, model, device, train_loader, optimizer, epoch, lr_scheduler, mas
         optimizer.zero_grad()
         output = model(data)
 
-        loss = F.nll_loss(output, target)
+        # rana:
+#        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
 
         if args.fp16:
             optimizer.backward(loss)
         else:
             loss.backward()
 
-        if mask is not None: mask.step()
-        else: optimizer.step()
+        if mask is not None:
+            mask.step()
+        else:
+            optimizer.step()
 
         if batch_idx % args.log_interval == 0:
             print_and_log('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader)*args.batch_size,
-                100. * batch_idx / len(train_loader), loss.item()))
+                epoch, batch_idx * len(data), len(train_loader) * args.batch_size,
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+def train_net():
+    mobilenet.train()
+    train_loss = 0
+    n_correct = 0
+    n_total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        # Training
+        optimizer.zero_grad()
+        outputs = mobilenet(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        # Recording process
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        n_correct += predicted.eq(targets).sum().item()
+        n_total += targets.shape[0]
+    return train_loss / (batch_idx + 1), n_correct / n_total
+
 
 def evaluate(args, model, device, test_loader, is_test_set=False):
     model.eval()
@@ -110,8 +172,8 @@ def evaluate(args, model, device, test_loader, is_test_set=False):
             if args.fp16: data = data.half()
             model.t = target
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             n += target.shape[0]
 
@@ -122,10 +184,12 @@ def evaluate(args, model, device, test_loader, is_test_set=False):
         test_loss, correct, n, 100. * correct / float(n)))
     return correct / float(n)
 
+
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
         shutil.copyfile(filename, 'model_best.pth.tar')
+
 
 def main():
     # Training settings
@@ -145,7 +209,8 @@ def main():
     parser.add_argument('--seed', type=int, default=17, metavar='S', help='random seed (default: 17)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--optimizer', type=str, default='sgd', help='The optimizer to use. Default: sgd. Options: sgd, adam.')
+    parser.add_argument('--optimizer', type=str, default='sgd',
+                        help='The optimizer to use. Default: sgd. Options: sgd, adam.')
     parser.add_argument('--save-model', type=str, default='./models/model.pt', help='For Saving the current Model')
     parser.add_argument('--data', type=str, default='mnist')
     parser.add_argument('--decay_frequency', type=int, default=25000)
@@ -156,11 +221,15 @@ def main():
     parser.add_argument('--start-epoch', type=int, default=1)
     parser.add_argument('--model', type=str, default='')
     parser.add_argument('--l2', type=float, default=5.0e-4)
-    parser.add_argument('--iters', type=int, default=1, help='How many times the model should be run after each other. Default=1')
-    parser.add_argument('--save-features', action='store_true', help='Resumes a saved model and saves its feature data to disk for plotting.')
-    parser.add_argument('--bench', action='store_true', help='Enables the benchmarking of layers and estimates sparse speedups')
+    parser.add_argument('--iters', type=int, default=1,
+                        help='How many times the model should be run after each other. Default=1')
+    parser.add_argument('--save-features', action='store_true',
+                        help='Resumes a saved model and saves its feature data to disk for plotting.')
+    parser.add_argument('--bench', action='store_true',
+                        help='Enables the benchmarking of layers and estimates sparse speedups')
     parser.add_argument('--max-threads', type=int, default=10, help='How many threads to use for data loading.')
-    parser.add_argument('--decay-schedule', type=str, default='cosine', help='The decay schedule for the pruning rate. Default: cosine. Choose from: cosine, linear.')
+    parser.add_argument('--decay-schedule', type=str, default='cosine',
+                        help='The decay schedule for the pruning rate. Default: cosine. Choose from: cosine, linear.')
     sparselearning.core.add_sparse_args(parser)
 
     args = parser.parse_args()
@@ -178,15 +247,16 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     print_and_log('\n\n')
-    print_and_log('='*80)
+    print_and_log('=' * 80)
     torch.manual_seed(args.seed)
     for i in range(args.iters):
-        print_and_log("\nIteration start: {0}/{1}\n".format(i+1, args.iters))
+        print_and_log("\nIteration start: {0}/{1}\n".format(i + 1, args.iters))
 
         if args.data == 'mnist':
             train_loader, valid_loader, test_loader = get_mnist_dataloaders(args, validation_split=args.valid_split)
         else:
-            train_loader, valid_loader, test_loader = get_cifar10_dataloaders(args, args.valid_split, max_threads=args.max_threads)
+            train_loader, valid_loader, test_loader = get_cifar10_dataloaders(args, args.valid_split,
+                                                                              max_threads=args.max_threads)
 
         if args.model not in models:
             print('You need to select an existing model via the --model argument. Available models include: ')
@@ -197,15 +267,15 @@ def main():
             cls, cls_args = models[args.model]
             model = cls(*(cls_args + [args.save_features, args.bench])).to(device)
             print_and_log(model)
-            print_and_log('='*60)
+            print_and_log('=' * 60)
             print_and_log(args.model)
-            print_and_log('='*60)
+            print_and_log('=' * 60)
 
-            print_and_log('='*60)
+            print_and_log('=' * 60)
             print_and_log('Prune mode: {0}'.format(args.prune))
             print_and_log('Growth mode: {0}'.format(args.growth))
             print_and_log('Redistribution mode: {0}'.format(args.redistribution))
-            print_and_log('='*60)
+            print_and_log('=' * 60)
 
         # add custom prune/growth/redisribution here
         if args.prune == 'magnitude_variance':
@@ -219,9 +289,10 @@ def main():
 
         optimizer = None
         if args.optimizer == 'sgd':
-            optimizer = optim.SGD(model.parameters(),lr=args.lr,momentum=args.momentum,weight_decay=args.l2, nesterov=True)
+            optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.l2,
+                                  nesterov=True)
         elif args.optimizer == 'adam':
-            optimizer = optim.Adam(model.parameters(),lr=args.lr,weight_decay=args.l2)
+            optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2)
         else:
             print('Unknown optimizer: {0}'.format(args.optimizer))
             raise Exception('Unknown optimizer.')
@@ -236,7 +307,7 @@ def main():
                 model.load_state_dict(checkpoint['state_dict'])
                 optimizer.load_state_dict(checkpoint['optimizer'])
                 print_and_log("=> loaded checkpoint '{}' (epoch {})"
-                      .format(args.resume, checkpoint['epoch']))
+                              .format(args.resume, checkpoint['epoch']))
                 print_and_log('Testing...')
                 evaluate(args, model, device, test_loader)
                 model.feats = []
@@ -245,22 +316,22 @@ def main():
             else:
                 print_and_log("=> no checkpoint found at '{}'".format(args.resume))
 
-
         if args.fp16:
             print('FP16')
             optimizer = FP16_Optimizer(optimizer,
-                                       static_loss_scale = None,
-                                       dynamic_loss_scale = True,
-                                       dynamic_loss_args = {'init_scale': 2 ** 16})
+                                       static_loss_scale=None,
+                                       dynamic_loss_scale=True,
+                                       dynamic_loss_args={'init_scale': 2 ** 16})
             model = model.half()
 
         mask = None
         if not args.dense:
             if args.decay_schedule == 'cosine':
-                decay = CosineDecay(args.prune_rate, len(train_loader)*(args.epochs))
+                decay = CosineDecay(args.prune_rate, len(train_loader) * (args.epochs))
             elif args.decay_schedule == 'linear':
-                decay = LinearDecay(args.prune_rate, len(train_loader)*(args.epochs))
-            mask = Masking(optimizer, decay, prune_rate=args.prune_rate, prune_mode=args.prune, growth_mode=args.growth, redistribution_mode=args.redistribution,
+                decay = LinearDecay(args.prune_rate, len(train_loader) * (args.epochs))
+            mask = Masking(optimizer, decay, prune_rate=args.prune_rate, prune_mode=args.prune, growth_mode=args.growth,
+                           redistribution_mode=args.redistribution,
                            verbose=args.verbose, fp16=args.fp16)
             mask.add_module(model, density=args.density)
 
@@ -273,16 +344,18 @@ def main():
 
             save_checkpoint({'epoch': epoch + 1,
                              'state_dict': model.state_dict(),
-                             'optimizer' : optimizer.state_dict()},
+                             'optimizer': optimizer.state_dict()},
                             is_best=False, filename=args.save_model)
 
             if not args.dense and epoch < args.epochs:
                 mask.at_end_of_epoch()
 
-            print_and_log('Current learning rate: {0}. Time taken for epoch: {1:.2f} seconds.\n'.format(optimizer.param_groups[0]['lr'], time.time() - t0))
+            print_and_log('Current learning rate: {0}. Time taken for epoch: {1:.2f} seconds.\n'.format(
+                optimizer.param_groups[0]['lr'], time.time() - t0))
 
         evaluate(args, model, device, test_loader, is_test_set=True)
-        print_and_log("\nIteration end: {0}/{1}\n".format(i+1, args.iters))
+        print_and_log("\nIteration end: {0}/{1}\n".format(i + 1, args.iters))
+
 
 if __name__ == '__main__':
-   main()
+    main()

sparselearning/models.py
@@ -555,13 +555,15 @@ class DepthWiseBlock(nn.Module):
 
 
 class MobileNet(nn.Module):
-    def __init__(self, widen_factor=1.0, num_classes=1000, prelu=False, save_features=False, bench_model=False):  #rana: save_features=False, bench_model=False not is use
+    def __init__(self, widen_factor=1.0, num_classes=1000, prelu=False, save_features=False, bench_model=False):  #rana: save_features and bench_model parameters is use
         """ Constructor
         Args:
             widen_factor: config of widen_factor
             num_classes: number of classes
         """
         super(MobileNet, self).__init__()
+        #rana:
+        self.bench = None if not bench_model else SparseSpeedupBench()
 
         block = DepthWiseBlock
 
@@ -638,50 +640,3 @@ def mobilenet(widen_factor=1.0, num_classes=1000):
     model = MobileNet(widen_factor=widen_factor, num_classes=num_classes)
     return model
 
-
-def mobilenet_2():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=2.0, num_classes=1000)
-    return model
-
-
-def mobilenet_1():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=1.0, num_classes=1000)
-    return model
-
-
-def mobilenet_075():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=0.75, num_classes=1000)
-    return model
-
-
-def mobilenet_05():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=0.5, num_classes=1000)
-    return model
-
-
-def mobilenet_025():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=0.25, num_classes=1000)
-    return model
-
-
-def mobilenet_1prelu():
-    """
-    Construct MobileNet.
-    """
-    model = MobileNet(widen_factor=1.0, num_classes=1000, prelu=True)
-    return model