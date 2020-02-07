fix loss calculation

mnist_cifar/main.py
@@ -141,7 +141,7 @@ def train(args, model, device, train_loader, optimizer, epoch, lr_scheduler, mas
                        100. * batch_idx / len(train_loader), loss.item()))
 
 
-
+""" rana
 def evaluate(args, model, device, test_loader, is_test_set=False):
     model.eval()
     test_loss = 0
@@ -164,6 +164,50 @@ def evaluate(args, model, device, test_loader, is_test_set=False):
         'Test evaluation' if is_test_set else 'Evaluation',
         test_loss, correct, n, 100. * correct / float(n)))
     return correct / float(n)
+"""
+def evaluate(args, model, device, test_loader, is_test_set=False):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    n = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            if args.fp16: data = data.half()
+            model.t = target
+            output = model(data)
+            #rana: test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target).item() *100
+            #pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            _, predicted = output.max(1)
+            #correct += pred.eq(target.view_as(pred)).sum().item()
+            correct += predicted.eq(target).sum().item()
+            n += target.shape[0]
+    test_loss /= float(n)
+
+    print_and_log('\n{}: Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)\n'.format(
+        'Test evaluation' if is_test_set else 'Evaluation',
+        test_loss, correct, n, 100. * correct / float(n)))
+    return correct / float(n)
+
+def get_loss_acc(is_test_dataset = True):
+    mobilenet.eval()
+    dataloader = testloader if is_test_dataset else trainloader
+    n_correct = 0
+    n_total = 0
+    test_loss = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+#         Recording process
+            outputs = mobilenet(inputs)
+            test_loss += criterion(outputs, targets).item()
+            _, predicted = outputs.max(1)
+            n_correct += predicted.eq(targets).sum().item()
+            n_total += targets.shape[0]
+    return test_loss/(batch_idx+1),n_correct/n_total
+
+
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):