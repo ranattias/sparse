4dfc018f43cc89813bc0f7947db6bf6b31ed6153
ranattias <ranatias@gmail.com>
2020-02-08 03:09:56 +0200

    new mobilenet -sequencil

--- mnist_cifar/main.py
+++ mnist_cifar/main.py
@@ -122,7 +122,7 @@ def train(args, model, device, train_loader, optimizer, epoch, lr_scheduler, mas
         output = model(data)
 
         # rana:
-#        loss = F.nll_loss(output, target)
+        #loss = F.nll_loss(output, target)
         loss = criterion(output, target)
 
         if args.fp16:
--- sparselearning/models.py
+++ sparselearning/models.py
@@ -510,6 +510,68 @@ class NetworkBlock(nn.Module):
         return x
 
 
+class MobileNet(nn.Module):
+    def __init__(self, widen_factor=1.0, num_classes=1000, prelu=False, save_features=False, bench_model=False):
+        super(MobileNet, self).__init__()
+        self.save_features = save_features
+        self.feats = []
+        self.densities = []
+        self.bench = None if not bench_model else SparseSpeedupBench()
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.features = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(1),
+        )
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        for layer_id, layer in enumerate(self.features):
+            if self.bench is not None and isinstance(layer, nn.Conv2d):
+                x = self.bench.forward(layer, x, layer_id)
+            else:
+                x = layer(x)
+
+            if self.save_features:
+                if isinstance(layer, nn.ReLU):
+                    self.feats.append(x.clone().detach())
+                if isinstance(layer, nn.Conv2d):
+                    self.densities.append((layer.weight.data != 0.0).sum().item()/layer.weight.numel())
+
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
+
 
 
 """ 
@@ -543,6 +605,7 @@ class DepthWiseBlock(nn.Module):
             self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
+        
         out = self.conv_dw(x)
         out = self.bn_dw(out)
         out = self.relu(out)
@@ -554,7 +617,8 @@ class DepthWiseBlock(nn.Module):
         return out
 
 
-class MobileNet(nn.Module):
+
+class MobileNet_delete (nn.Module):
     def __init__(self, widen_factor=1.0, num_classes=1000, prelu=False, save_features=False, bench_model=False):  #rana: save_features and bench_model parameters is use
         """ Constructor
         Args:
